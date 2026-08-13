bcf88ad871d095bea6948dd37e1b25247db1f4f9
anatoly <anatole.work@gmail.com>
2026-08-13 15:06:56 +0300

    hw06-reflection, homework

---
+++ hw06-reflection/src/main/java/ru/otus/anatoly/Hw06Reflection.java
@@ -0,0 +1,11 @@
+package ru.otus.anatoly;
+
+import ru.otus.anatoly.runner.TestRunner;
+import ru.otus.anatoly.testsample.CarTest;
+
+public class Hw06Reflection {
+    
+    public static void runHomeWork() {
+        TestRunner.run(CarTest.class);
+    }
+}
